e4e771f45b3787a4641ec7bafe9682f3f69a50fe
Ibrahim Jubran <ibrahim@orb-ar.com>
2024-12-08 09:14:25 +0200

    ..

--- src/virgin_vm_comfy3d.sh
+++ src/virgin_vm_comfy3d.sh
@@ -17,7 +17,6 @@ lspci | grep -i nvidia
 nvidia-smi
 # install nvidia drivers and CUDA
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-ubuntu2204.pin
-sudo mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600
 wget https://developer.download.nvidia.com/compute/cuda/12.4.0/local_installers/cuda-repo-ubuntu2204-12-4-local_12.4.0-535.86.10-1_amd64.deb
 sudo dpkg -i cuda-repo-ubuntu2204-12-4-local_12.4.0-535.86.10-1_amd64.deb
 sudo cp /var/cuda-repo-ubuntu2204-12-4-local/cuda-*-keyring.gpg /usr/share/keyrings/
