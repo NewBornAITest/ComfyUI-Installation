941bed145ccde60e1a0dd3996beeeaaa8655020b
Ibrahim Jubran <ibrahim@orb-ar.com>
2024-12-08 09:27:14 +0200

    minor cuda installation changes

--- src/virgin_vm_comfy3d.sh
+++ src/virgin_vm_comfy3d.sh
@@ -17,11 +17,12 @@ lspci | grep -i nvidia
 nvidia-smi
 # install nvidia drivers and CUDA
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-ubuntu2204.pin
-wget https://developer.download.nvidia.com/compute/cuda/12.4.0/local_installers/cuda-repo-ubuntu2204-12-4-local_12.4.0-535.86.10-1_amd64.deb
-sudo dpkg -i cuda-repo-ubuntu2204-12-4-local_12.4.0-535.86.10-1_amd64.deb
+sudo mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget https://developer.download.nvidia.com/compute/cuda/12.4.0/local_installers/cuda-repo-ubuntu2204-12-4-local_12.4.0-550.54.14-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu2204-12-4-local_12.4.0-550.54.14-1_amd64.deb
 sudo cp /var/cuda-repo-ubuntu2204-12-4-local/cuda-*-keyring.gpg /usr/share/keyrings/
 sudo apt-get update
-sudo apt-get -y install cuda
+sudo apt-get -y install cuda-toolkit-12-4
 
 source .bashrc
 source .bashrc
